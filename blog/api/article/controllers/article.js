bb7dc3fd711f04eb3fb4935607b0df5f0f87219a
BenoitRobin <benoitrobin@me.com>
2020-11-17 16:44:30 +0100

    add categoryName route

--- blog/api/article/controllers/article.js
+++ blog/api/article/controllers/article.js
@@ -1,8 +1,21 @@
 'use strict';
 
+const { default: createStrapi } = require("strapi");
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findByCategoryName(ctx) {
+    // const categoryName = ctx.params.categoryName;
+    const { categoryName } = ctx.params;
+
+    // Maintenat que l'on a le nom de la cat, il faut faire une requête active record pour rechercher les articles ciblés
+    const results = await strapi.query('article').find({
+      "categories.name_contains": categoryName
+    });
+    return results;
+    }
+};
